ce09f35d818d20d76410f098d9573ca4c9f6cb44
Ada Pedersen <email@adapdr.me>
2023-09-15 13:10:05 +0200

    updated tests tbased on feedback
    - updated tests to account for the change on lowercase and alphabetical sub-ordering
    - added extra test

--- tests.py
+++ tests.py
@@ -14,16 +14,27 @@ class WordAnalyzerTests(TestCase):
     def setUp(self):
         """Sets up test variables"""
         self.sample_text = "Beep beep beep, Boop boop, blip blop"
-        self.sample_list = [("beep", 2), ("Beep", 1)]
+        self.sample_list = [("beep", 3), ("boop", 2)]
 
     def test_max_frequency(self):
         max_frequency = WA.max_frequency(self.sample_text)
-        self.assertEqual(2, max_frequency)
+        self.assertEqual(3, max_frequency)
 
     def test_get_frequency(self):
-        frequency = WA.get_frequency(self.sample_text, "beep")
+        frequency = WA.get_frequency(self.sample_text, "boop")
         self.assertEqual(2, frequency)
 
     def test_get_frequency_listed(self):
         frequency_listed = WA.get_frequency_listed(self.sample_text, 2)
         self.assertEqual(self.sample_list, frequency_listed)
+
+    def test_extra(self):
+        """
+        For input text “The sun shines over the lake” and n = 3,
+        it should return the list { (“the”, 2), (“lake”, 1), (“over”, 1) }
+        """
+        most_frequent_n_words = WA.get_frequency_listed(
+            "The sun shines over the lake", 3
+        )
+        expected_assignment_example = [("the", 2), ("lake", 1), ("over", 1)]
+        self.assertEqual(expected_assignment_example, most_frequent_n_words)
